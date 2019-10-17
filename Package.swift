24a53a2945a3f764df8a155eb4c3408bb8243eba
AthanasiusOfAlex <lmelahn@legionaries.org>
2019-10-17 19:15:35 +0200

    Updated package.swift to modern version.

--- Package.swift
+++ Package.swift
@@ -1,3 +1,6 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 //
 //  Async.swift
 //
@@ -23,9 +26,28 @@
 //	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
 import PackageDescription
 
 let package = Package(
-    name: "Async"
+    name: "Async",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Async",
+            targets: ["Async"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Async",
+            dependencies: []),
+//        .testTarget(
+//            name: "AsyncTests",
+//            dependencies: ["Async"]),
+    ]
 )
